ed7e43b368bc108328e307f60850a46a08668aaa
chenjie <1424332438@qq.com>
2020-05-14 21:05:01 +0800

    初次提交

---
+++ spring-boot-data-save/src/main/java/com/spring/boot/dataSave/SpringDataSaveApplication.java
@@ -0,0 +1,17 @@
+package com.spring.boot.dataSave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ * @author chenjie
+ * @date 2020-05-14
+ */
+@SpringBootApplication
+public class SpringDataSaveApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(SpringDataSaveApplication.class, args);
+    }
+}
