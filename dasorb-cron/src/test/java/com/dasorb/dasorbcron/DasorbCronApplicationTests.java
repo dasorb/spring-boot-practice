e283e60d987ecbc1c1090d104b7120f2ab3bfda8
chenjie <1424332438@qq.com>
2020-08-19 21:18:17 +0800

    修改提交

--- dasorb-cron/src/test/java/com/dasorb/dasorbcron/DasorbCronApplicationTests.java
+++ dasorb-cron/src/test/java/com/dasorb/dasorbcron/DasorbCronApplicationTests.java
@@ -2,12 +2,22 @@ package com.dasorb.dasorbcron;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import sun.security.action.GetPropertyAction;
+
+import java.io.BufferedWriter;
+import java.security.AccessController;
+import java.util.Map;
 
 @SpringBootTest
 class DasorbCronApplicationTests {
 
     @Test
     void contextLoads() {
+        String aaa = AccessController.doPrivileged(
+                new GetPropertyAction("line.separator"));
+        GetPropertyAction getPropertyAction = new GetPropertyAction("line.separator");
+        String run = getPropertyAction.run();
+        System.out.println("测试" + run + "测试");
     }
 
 }
